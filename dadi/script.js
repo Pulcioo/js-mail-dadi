9472fb9bb5e4df27a91a8cf802b641573c58aa1d
Davide Pulci <davide.pulci@gmail.com>
2022-03-09 16:37:32 +0100

    generati numeri random e stampati in console e document

--- dadi/script.js
+++ dadi/script.js
@@ -1,1 +1,14 @@
-console.log('JS OK')
+// genero numero random per l'utente
+const numberUser = Math.floor(Math.random() * 6) + 1;
+
+console.log(numberUser)
+
+// genero numero random per il computer
+const numberComputer = Math.floor(Math.random() * 6) + 1;
+
+console.log(numberComputer)
+
+// stampo entrambi i numeri in document 
+document.getElementById('n-user').innerHTML = "Hai totalizzato " + numberUser;
+
+document.getElementById('n-computer').innerHTML = "Il computer ha totalizzato " + numberComputer;
