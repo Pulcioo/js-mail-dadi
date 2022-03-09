7698dd2ac0b6ca33684bf3e20a72943efc5d87f3
Davide Pulci <davide.pulci@gmail.com>
2022-03-09 16:01:09 +0100

    completata parte mail

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log('JS OK')
+
+// chiedere all'utente la propria mail
+const mailUser = prompt('Inserisci la tua e-mail qui sotto!')
+
+console.log(mailUser)
+
+// lista mail registrate
+const mailWithAccess = ["davide.pulci@gmail.com", "francesco.totti@gmail.com", "diego.maradona@gmail.com", "cristiano.ronaldo@gmail.com"]
+
+console.log(mailWithAccess)
+
+// controllare che la mail inserita sia nella lista di chi può accedere oppure no
+let verified = false;
+
+for (let i = 0; i < mailWithAccess.length; i++) {
+    if (mailUser == mailWithAccess[i]) {
+        verified = true;
+    } if (verified == true) {
+        document.getElementById('your-email').innerHTML = "Accesso consentito ✔︎";
+    } else {
+        document.getElementById('your-email').innerHTML = "Accesso negato ✘";
+    }
+}
